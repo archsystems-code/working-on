68971c14db5941e945124455b79b1b2619c9db5a
Will Smith <lwilsonsmith@gmail.com>
2018-04-25 01:33:17 -0400

    Revert previous and try another solution

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 
 	// Setup schedule digest jobs
 	for _, i := range digestConfig.Items {
-		digestJob := postDigest(i.channel, os.Getenv(i.token), i.tags)
+		digestJob := postDigest(i.Channel, os.Getenv("BOT_TOKEN"), i.Tags)
 		_, err := scheduler.Every().Day().At(digestTime).Run(digestJob)
 		if err != nil {
 			log.Infoln(err)
